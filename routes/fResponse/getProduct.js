97c1762febe324d8946fe04160063164a21c76e6
ghcm <take_it_easy_js@yahoo.com>
2014-04-06 21:04:26 +0400

    getProductById refactored

--- routes/fResponse/getProduct.js
+++ routes/fResponse/getProduct.js
@@ -2,6 +2,8 @@
 var url = require('url');
 var fs = require("fs");
 
+Good = require('../../models/good').Good;
+
 exports.get = function(req, res){
 
     var url_parts = url.parse(req.url, true);
@@ -12,10 +14,17 @@ exports.get = function(req, res){
         });
     }
     else {
-        fs.readFile(__dirname + "/json/getProductPizza.json", "utf8", function(err, data) {
+
+          console.log(url_parts.query);
+
+           Good.find({"_id": url_parts.query.pizzaProductId}, function(err, result) {
+                                                                               res.json(result[0]);
+           });
+
+        /*fs.readFile(__dirname + "/json/getProductPizza.json", "utf8", function(err, data) {
             if (err) throw err;
             res.json(JSON.parse(data));
-        });
+        });*/
     }
 };
 
