4b2d8fe1fc0068bd67cb4977731ff37f25fdcc57
ghcm <take_it_easy_js@yahoo.com>
2014-04-14 13:26:23 +0400

    grunt refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,43 +2,61 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        concat: {
+     /*   concat: {
             "options": { "separator": ";" },
             "build": {
-                "src": ["public/f/lib/angular/angular.js","public/f/js/angular-webstorage.js","public/f/js/app_dev.js", "public/f/js/services.js", "public/f/js/controllers.js", "public/f/js/filters.js", "public/f/js/directives.js", "public/f/lib/angular/angular-resource.js", "public/f/js/bootstrap.js", "public/f/js/custom.js"],
+                "src": [
+                    "public/f/lib/angular/angular.js",
+                    "public/f/js/angular-webstorage.js",
+                    "public/f/js/app_dev.js",
+                    "public/f/js/services.js",
+                    "public/f/js/controllers.js",
+                    "public/f/js/filters.js",
+                    "public/f/js/directives.js",
+                    "public/f/lib/angular/angular-resource.js",
+                    "public/f/js/bootstrap.js",
+                    "public/f/js/custom.js"
+                ],
                 "dest": "public/f/js/grunt.js"
             }
-        },
+        },*/
         uglify: {
-            js: {
-
-            },
             options: {
                 // the banner is inserted at the top of the output
-                banner: '/*! <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                banner: '/*! <%= grunt.template.today("dd-mm-yyyy hh-MM") %> */\n',
+                //is need to change functions names
                 mangle: false
             },
             dist: {
                 files: {
-                    'public/f/js/grunt.min.js': ['<%= concat.build.dest %>']
+                    'public/f/js/min/grunt.min.js': [
+                        "public/f/lib/angular/angular.js",
+                        "public/f/js/angular-webstorage.js",
+                        "public/f/js/app_dev.js",
+                        "public/f/js/services.js",
+                        "public/f/js/controllers.js",
+                        "public/f/js/filters.js",
+                        "public/f/js/directives.js",
+                        "public/f/js/bootstrap.js",
+                        "public/f/js/custom.js",
+                        "public/f/lib/angular/angular-resource.js"
+                    ]
                 }
-            },
-            prod: {
-                options: {
-                    compress: false
-                }
-            },
-            dev: {
-                options: {
-                    compress: false
-                }
-            }                         ,
-
-    }});
+            }
+        },
+        qunit: {
+            files: ['public/f/js/tests/*.html']
+        },
+        gitpush: {
+            
+        }
+    });
 
     // Load required modules
-    grunt.loadNpmTasks('grunt-contrib-concat');
+   // grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-git');
     // Task definitions
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['uglify', 'qunit']);
 };
---
+++ public/f/js/tests/tests.js
@@ -0,0 +1,3 @@
+test( "Is 1 equal for 1", function() {
+    equal( 1, 1 );
+});
